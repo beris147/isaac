df02570617ce1a3c5ddeb7b328b40d582ec5c4d8
beris147 <edwir147@gmail.com>
2022-05-08 12:48:06 -0700

    [session] add firebase project

---
+++ packages/session/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyA0zcqN1O8CoxIN-E3SNLb9y415jn8S7Fs',
+	authDomain: 'isaac-86bfe.firebaseapp.com',
+	projectId: 'isaac-86bfe',
+	storageBucket: 'isaac-86bfe.appspot.com',
+	messagingSenderId: '556478845500',
+	appId: '1:556478845500:web:e75170ca2e168e49e42939',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
