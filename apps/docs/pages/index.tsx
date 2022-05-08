786f660d6111ec78126cc9aa28255021e3b7eb47
beris147 <edwir147@gmail.com>
2022-05-08 12:40:27 -0700

    [documentation] add description of apps

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Docs() {
-  return (
-    <div>
-      <h1>Docs</h1>
-      <Button />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Docs</h1>
+			<p>Here we will put the code documentation</p>
+		</div>
+	);
 }
